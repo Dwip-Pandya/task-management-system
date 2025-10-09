0ab3879ef048c9246729c29e4a2c8e5d9c2a6b97
Dwip-Pandya <210020107044@aitindia.in>
2025-10-09 16:50:11 +0530

    dynamic status change - admin

--- resources/views/admin/tasks/index.blade.php
+++ resources/views/admin/tasks/index.blade.php
@@ -164,6 +164,68 @@
     </div>
 </div>
 @push('scripts')
-<script src="{{ asset('js/task.js') }}"></script>
+<script>
+    $(document).ready(function() {
+
+        // Assigned User
+        $('.change-assigned').change(function() {
+            let id = $(this).data('id');
+            let assigned_to = $(this).val();
+            $.ajax({
+                url: '/admin/tasks/' + id + '/update-assigned',
+                method: 'POST',
+                data: {
+                    _token: "{{ csrf_token() }}",
+                    assigned_to: assigned_to
+                },
+                success: function(data) {
+                    if (data.success) {
+                        alert('Assigned user updated!');
+                    }
+                }
+            });
+        });
+
+        // Status
+        $('.change-status').change(function() {
+            let id = $(this).data('id');
+            let status_id = $(this).val();
+            $.ajax({
+                url: '/admin/tasks/' + id + '/update-status',
+                method: 'POST',
+                data: {
+                    _token: "{{ csrf_token() }}",
+                    status_id: status_id
+                },
+                success: function(data) {
+                    if (data.success) {
+                        alert('Status updated!');
+                        location.reload(); // optional: update badge color
+                    }
+                }
+            });
+        });
+
+        // Priority
+        $('.change-priority').change(function() {
+            let id = $(this).data('id');
+            let priority_id = $(this).val();
+            $.ajax({
+                url: '/admin/tasks/' + id + '/update-priority',
+                method: 'POST',
+                data: {
+                    _token: "{{ csrf_token() }}",
+                    priority_id: priority_id
+                },
+                success: function(data) {
+                    if (data.success) {
+                        alert('Priority updated!');
+                    }
+                }
+            });
+        });
+
+    });
+</script>
 @endpush
 @endsection
